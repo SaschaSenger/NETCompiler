0c4e0bb428a92da2dc18e6e3c1b5968b50737b6f
Sascha Senger <sascha.senger1@gmail.com>
2022-05-05 19:47:02 +0200

    First steps to generate project file

--- NETCompilerPlayground/NETCompilerPlayground/Class1.cs
+++ NETCompilerPlayground/NETCompilerPlayground/Class1.cs
@@ -1,7 +1,29 @@
-﻿namespace NETCompilerPlayground
+﻿using Microsoft.Build.Evaluation;
+//using Microsoft.CodeAnalysis;
+using Microsoft.Build.Construction;
+using System.Linq;
+
+namespace NETCompilerPlayground
 {
-    public class Class1
+    public class Compiler
     {
+        public Compiler()
+        {
+            Project testProj = new Project();
+            testProj.Xml.AddTarget("BuildProjects");
+
+            foreach (ProjectTargetElement pti in testProj.Xml.Targets.Where(pti => pti.Name == "BuildProjects"))
+            {
+                pti.AddTask("MSBuild");
+            }
+
+            testProj.Save(@"D:\testProj.csproj");
+
+            //var projectCollection = new ProjectCollection();
+            //var proj = projectCollection.LoadProject("Test");
+
+            //var tmp = new Microsoft.Build.Evaluation.Project();
+        }
 
     }
 }
